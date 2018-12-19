df10ec2479ee1aa79b172b5c85d8fc0626f7f3e6
sricagno <sricagno@AMANECER>
2018-12-19 11:25:11 -0300

    Compatibilidad logo en otro navegadores

--- js/site.js
+++ js/site.js
@@ -1,2 +1,3 @@
 ﻿//document.getElementById("brandImg").src = "../Images/TB OP5 V2.png";
-$("#brandImg").attr('src', 'Images/TB logo blanco.png');
+// $("#brandImg").attr('src', 'Images/TB logo blanco.png');
+// $("#brandImg").attr({ style: "content:url('Images/TB logo blanco.png')" });
